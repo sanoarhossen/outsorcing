efadcb14d15a5c87a33efd331f66bfe300f1e6c4
Md.Sanoar Hossen <101052674+shimul91@users.noreply.github.com>
2023-01-25 17:04:25 +0600

    Outsorcing
    Outsorcing

---
+++ script.js
@@ -0,0 +1,23 @@
+var swiper = new Swiper(".revew-slider", {
+    spaceBetween: 20,
+    centeredSlides: true,
+    autoplay: {
+      delay: 7500,
+      disableOnInteraction: false,
+    },
+  loop:true,
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    640: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+  });
